920b12004f7ba931ae13ed7e24d4e4c207e899f6
17122022117 <17122022117@proton.me>
2026-05-19 22:00:16 +0400

    add run_python_file

---
+++ functions/run_python_file.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        working_dir_comm = os.path.normpath(working_dir_abs)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    except Exception as e:
+        return f"Error: {e}"
+
+    if not valid_target_dir:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    elif not os.path.isfile(target_dir):
+        return f'Error: "{file_path}" does not exist or is not a regular file'
+    elif not target_dir.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file'
+    else:
+        print(f'Success: "{file_path}" is within the working directory')
+        command = ["python", target_dir]
+        if args is not None:
+            command.extend(args)
+        try:
+            process = subprocess.run(command, cwd=working_dir_comm, capture_output=True, text=True, timeout=30)
+        except Exception as e:
+            return f"Error: executing Python file: {e}"
+        string_list = []
+        if process.returncode != 0:
+            string_list.append(f'Process exited with code {process.returncode}')
+        elif process.stdout is None and process.stderr is None:
+            string_list.append(f'No output produced')
+        else:
+            string_list.append(f'STDOUT: {process.stdout}\nSTDERR: {process.stderr}')
+        return "".join(string_list)
+        
+            
